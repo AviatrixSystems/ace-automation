2f4525fc2b853323742721eecc3b1d339c1a4d1d
Selina Loggins <157069359+tatiLogg@users.noreply.github.com>
2026-03-17 12:02:41 -0500

    Update VPC IDs for AWS spoke configurations

--- lab1/main.tf
+++ lab1/main.tf
@@ -6,7 +6,7 @@ module "us_east_1_spoke" {
   instance_size    = "t3.small"
   use_existing_vpc = true
   gw_subnet        = "10.1.0.16/28"
-  vpc_id           = var.us_east_1_vpc_id
+  vpc_id           = "vpc-05395dc11b0ccf274"
   region           = "us-east-1"
   account          = var.account_name_aws
   ha_gw            = false
@@ -22,7 +22,7 @@ module "us_west_2_spoke" {
   instance_size    = "t3.small"
   use_existing_vpc = true
   gw_subnet        = "10.2.0.16/28"
-  vpc_id           = var.us_west_2_vpc_id
+  vpc_id           = "vpc-0f9456626d18f89e51"
   region           = "us-west-2"
   account          = var.account_name_aws
   ha_gw            = false
