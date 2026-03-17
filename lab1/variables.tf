89d329f50537f8c571123122d0603e7efc8d901d
Selina Loggins <157069359+tatiLogg@users.noreply.github.com>
2026-03-17 10:19:17 -0500

    Add default values for variables in variables.tf

--- lab1/variables.tf
+++ lab1/variables.tf
@@ -1,9 +1,11 @@
 variable "controller_ip" {
   description = "The Aviatrix controller eip"
+default     = "44.214.60.253"
 }
 
 variable "password" {
   description = "The Aviatrix platform student password"
+default     = "Selina123!"
 }
 
 variable "account_name_aws" {
@@ -13,8 +15,10 @@ variable "account_name_aws" {
 
 variable "us_east_1_vpc_id" {
   description = "The VPC ID for the us-east-1 region"
+default     = "vpc-05395dc11b0ccf274"
 }
 
 variable "us_west_2_vpc_id" {
   description = "The VPC ID for the us-west-2 region"
+default     = "vpc-0f9456626d18f89e51"
 }
