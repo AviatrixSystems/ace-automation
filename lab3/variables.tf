1fca496510422c9b21409e5bedc32d109742007c
Selina Loggins <157069359+tatiLogg@users.noreply.github.com>
2026-03-20 15:52:45 -0500

    Define types for AWS variables in variables.tf
    Added type definitions for AWS account name and VPC IDs.

--- lab3/variables.tf
+++ lab3/variables.tf
@@ -10,3 +10,14 @@ variable "account_name_aws" {
   description = "The onboarded Aviatrix access account name for AWS"
   default     = "aws-account"
 }
+variable "account_name_aws" {
+  type = string
+}
+
+variable "us_east_1_vpc_id" {
+  type = string
+}
+
+variable "us_west_2_vpc_id" {
+  type = string
+}
